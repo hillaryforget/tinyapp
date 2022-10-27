4f1f0270e93cbb9cbd99e92aa81e5c922975a033
Hillary Forget <hillaryforget@outlook.com>
2022-10-27 14:22:58 -0400

    add route to urlDatabase object

--- expressServer.js
+++ expressServer.js
@@ -1,16 +1,21 @@
-const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
+const express = require('express');
+const app = express(); //dependancy
+const PORT = 8080; //default port 8080
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+//add route
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
 });
