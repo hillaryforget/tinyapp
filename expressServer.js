efd74fcaa824d179a2165602f2022d24470f976d
Hillary Forget <hillaryforget@outlook.com>
2022-10-27 19:38:15 -0400

    add route urlsShow.ejs

--- expressServer.js
+++ expressServer.js
@@ -31,4 +31,9 @@ app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render('urlsShow', templateVars);
+});
+
 //
